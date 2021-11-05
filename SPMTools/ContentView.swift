7f5fbe2738f83a01bf5291a7cc1f84baeadd0791
king <3925@winnermedical.com>
2021-11-05 15:00:16 +0800

    Initial Commit

---
+++ SPMTools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPMTools
+//
+//  Created by 张行 on 2021/11/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SPMTools/SPMToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMToolsApp.swift
+//  SPMTools
+//
+//  Created by 张行 on 2021/11/5.
+//
+
+import SwiftUI
+
+@main
+struct SPMToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
